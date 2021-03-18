2a7e97b7e54707d86ade6a82bdc47affade6cdf2
Sharky <Sharky121@mail.ru>
2021-03-19 01:46:58 +0300

    Валидирует формы

--- js/map.js
+++ js/map.js
@@ -1,6 +1,31 @@
 /* global L:readonly */
-import {TokyoCoords, MapEvents, Pins, Tile, PRECISION} from './consts.js';
 import {createCardTemplate} from './card-template.js';
+
+const PRECISION = 5;
+const TokyoCoords = {
+  LAT: 35.681700,
+  LNG: 139.753882,
+}
+const MapEvents = {
+  LOAD: 'load',
+  MOVEEND: 'moveend',
+}
+const Pins = {
+  redPin: {
+    URL: 'img/main-pin.svg',
+    SIZE: [52, 52],
+    ANCHOR_SIZE: [26, 52],
+  },
+  bluePin: {
+    URL: 'img/pin.svg',
+    SIZE: [52, 52],
+    ANCHOR_SIZE: [26, 52],
+  },
+};
+const Tile = {
+  URL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}
 
 const createCustomPopup = (card, cardTemplate) => {
   return createCardTemplate(card, cardTemplate);
